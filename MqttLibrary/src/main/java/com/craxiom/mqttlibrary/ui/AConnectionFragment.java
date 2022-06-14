bd52b0d5fd738ccc679e250dbb4cc5c204c882e5
Dino Tufekcic <dtufekcic@ctic.us>
2022-06-14 14:48:54 -0700

    Remove unused import

--- MqttLibrary/src/main/java/com/craxiom/mqttlibrary/ui/AConnectionFragment.java
+++ MqttLibrary/src/main/java/com/craxiom/mqttlibrary/ui/AConnectionFragment.java
@@ -21,7 +21,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
